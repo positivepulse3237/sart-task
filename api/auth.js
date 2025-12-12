cea7271ccf3bfc3da7b504f6be404b33354b8707
positivepulse3237 <positivepulse3237@gmail.com>
2025-12-12 16:04:57 +0600

    Delete api directory

--- api/auth.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const { pwd } = req.query;
-  if (pwd === process.env.ADMIN_PASSWORD) {
-    return res.status(200).json({ success: true });
-  }
-  return res.status(403).json({ error: 'Unauthorized' });
-}
